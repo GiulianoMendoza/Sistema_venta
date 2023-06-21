93f42e89c0cc206cc37147074b72ea2adfe23b15
Giuliano Mendoza <giulianovictorm98@gmail.com>
2023-06-21 00:20:24 -0300

    Creando la interfaz de categorias

---
+++ CapaDatos/Conexion.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class Conexion
+    {
+        private string BaseDato;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            this.BaseDato = "BD_Prana";
+            this.Servidor = "DESKTOP-G1GT125\\SQLEXPRESS";
+            this.Usuario = "Prana";
+            this.Clave = "123456";
+            this.Seguridad = false;
+        }
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.BaseDato + ";";
+                if (Seguridad)
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
+                }
+                else
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User Id=" + this.Usuario + "; Password=" + this.Clave;
+                }
+            }
+            catch(Exception e)
+            {
+                Cadena = null;
+                throw e;
+            }
+            return Cadena;
+        }
+        public static Conexion getInstancia()
+        {
+            if(Con== null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
---
+++ Prana/Frm_Categorias.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prana
+{
+    public partial class Frm_Categorias : Form
+    {
+        public Frm_Categorias()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Categorias_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
